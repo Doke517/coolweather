60528453ac544687c199bc08911e8b7d4893cb4a
JT517 <dsa9292@hotmail.com>
2018-04-02 17:39:52 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/example/jt0930517/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/jt0930517/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.jt0930517.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.jt0930517.coolweather.gson.Forecast;
 import com.example.jt0930517.coolweather.gson.Weather;
+import com.example.jt0930517.coolweather.service.AutoUpdateService;
 import com.example.jt0930517.coolweather.util.HttpUtil;
 import com.example.jt0930517.coolweather.util.Utility;
 
@@ -158,6 +160,8 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather",responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+                            Intent intent = new Intent(WeatherActivity.this, AutoUpdateService.class);
+                            startService(intent);
                         }else{
                             Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
                         }
